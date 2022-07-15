c35f01c0c6b6c9fd6d6f394ece82113cd4f1b2f1
dbonafin <demianbonafina@gmail.com>
2022-07-15 13:01:19 +0200

    reset done, images and font added, analysis done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <!-- Common -->
+    <!-- Header nav and footer medical departments will be dynamic (props) -->
+    <!-- Header, footer, 3 columns section with different bg colors - width 80% margin 0 auto -->
+    <!-- All other sections - width  60% margin 0 auto  -->
+
+    <!-- Here will be the header section (header+jumbotron) -->
+    <header>
+      <PageHeader msg="Reset done" />
+    </header>
+
+    <!-- Here will be the main section -->
+    <main>
+      <!-- Welcome to avada intro section -->
+      <!-- title, text and 4 columns (img, title, text) -->
+
+      <!-- Meet our doctors section -->
+      <!-- icon, title, text and 3 doctor info cards (picture, title, text, socials) -->
+
+      <!-- Our health services section -->
+      <!-- icon, title, text and 3 columns(X2) (img, title, text) -->
+
+      <!-- Tour our facilities -->
+      <!-- icon, title, :after div, text and play image -->
+
+      <!-- 3 columns section with different bg colors -->
+      <!-- title, image, text and "button" -->
+
+      <!-- Make an appointment section -->
+      <!-- title, :after div, text, info request form and "button" -->
+
+      <!-- Avada partners section -->
+      <!-- Brand partners to show while scrolling (overflow x) -->
+    </main>
+
+    <!-- Here will be the footer section -->
+    <footer>
+      <!-- Top footer (three columns) -->
+
+      <!-- Bottom footer (ul inline-block) -->
+    </footer>
+
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import PageHeader from './components/PageHeader.vue';
+
+  export default {
+    name: 'App',
+    components: { PageHeader}
   }
-}
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  @import "./style/common.scss"
+
 </style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'ResetDone',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+
+</style>
