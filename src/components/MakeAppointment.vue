227888e051d10fa92cd57d76dc5bf07cbd77ee76
dbonafin <demianbonafina@gmail.com>
2022-07-18 13:47:40 +0200

    make appointment component done

--- src/components/MakeAppointment.vue
+++ src/components/MakeAppointment.vue
@@ -1,9 +1,9 @@
 <template>
     
-   <div class="facilities-wrapper">
+   <div class="appointment-wrapper">
         <div class="small-container">
 
-            <!-- Top area of the doctors section -->
+            <!-- Top area of the appointment section -->
             <div class="top-area">
 
                 <h2>make an appointment</h2>
@@ -14,45 +14,77 @@
 
                 <div class="appointment-form">
                     <form>
-                        <div class="form-row">
-                            
-                            <div class="form-group col-md-6">
-                                <label for="inputPassword4 sr-only" >Name</label>
-                                <input type="text" class="form-control" id="inputName" placeholder="Name">
-                            </div>
 
-                            <div class="form-group col-md-6">
-                                <label for="inputEmail4">Email</label>
-                                <input type="email" class="form-control" id="inputEmail" placeholder="Email">
-                            </div>
-
-                        </div>
+                        <!-- Input group -->
 
                         <div class="form-row">
                             
-                            <div class="form-group col-md-6">
-                                <label for="inputPassword4 sr-only" >Name</label>
-                                <input type="text" class="form-control" id="inputName" placeholder="Name">
+                            <div class="form-group">
+                                <label for="inputName">Name</label>
+                                <input 
+                                    type="text" 
+                                    class="form-control" 
+                                    id="inputName" 
+                                    placeholder="Name*"
+                                >
                             </div>
 
-                            <div class="form-group col-md-6">
-                                <label for="inputEmail4">Email</label>
-                                <input type="email" class="form-control" id="inputEmail" placeholder="Email">
-                            </div>
-                            
-                        </div>
-
-                        <div class="form-row">
-                            
-                            <div class="form-group col-md-6">
+                            <div class="form-group">
                                 <label for="inputEmail">Email</label>
-                                <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                                <input 
+                                    type="email" 
+                                    class="form-control" 
+                                    id="inputEmail" 
+                                    placeholder="Email*"
+                                >
+                            </div>
+
+                        </div>
+
+                        <div class="form-row">
+                            
+                            <div class="form-group">
+                                <label for="inputNumber">Phone Number</label>
+                                <input 
+                                    type="text" 
+                                    class="form-control" 
+                                    id="inputNumber" 
+                                    placeholder="Phone Number"
+                                >
+                            </div>
+
+                            <div class="form-group">
+                                <label for="inputDate">Date</label>
+                                <input 
+                                    type="date" 
+                                    class="form-control" 
+                                    id="inputDate"
+                                    placeholder="Appointment Date"
+                                >
                             </div>
                             
                         </div>
-                       
-                        
-                        <button type="submit" class="btn color-btn">Sign in</button>
+
+                        <div>
+                            <!-- Request info text area -->
+                            <div class="form-group form-request">
+                                <label for="formTextArea">TextArea</label>
+
+                                <textarea 
+                                    class="form-control" 
+                                    id="formTextArea" 
+                                    rows="3"
+                                    placeholder="How can we help?*">
+                                </textarea>
+                            </div>
+                            
+                        </div>
+
+                        <!-- Submit button -->
+                        <span class="btn color-btn">
+                            <a href="#">make an appointment</a>
+                        </span>
+                         
                     </form>
                 </div>
             </div>
@@ -74,7 +106,7 @@
 <style lang="scss" scoped>
 
     @import "../style/common.scss";
-    .facilities-wrapper {
+    .appointment-wrapper {
         background-image: url('../assets/images/make-an-appointment.jpg');
         background-position: center;
         background-size: cover;      
@@ -88,9 +120,6 @@
         width: 60%;
         margin: 0 auto;
         text-align: center;
-        img{
-            width: 60px;
-        }
         h2 {
             text-transform: capitalize;
             font-size: 30px;
@@ -105,14 +134,30 @@
             }
         }
         .appointment-form{
+            margin-top: 20px;
             label {
                 display: none;
             }
-            input {
-                width: 300px;
-                margin: 10px;
+            input, textarea {
+                width: 100%;
+                margin: 10px 0;
+                padding: 2px 5px;
                 background-color: transparent;
                 border: 1px solid $txt-secondary-color;
+                color: $txt-secondary-color;
+                opacity: calc(0.6);
+                font-size: 10px;
+            }
+            .form-group {
+                width: calc((100% / 2) - 10px);
+                ::placeholder {
+                    padding: 10px;
+                    color: $txt-secondary-color;
+                    opacity: calc(0.6);
+                }
+            }
+            .form-request {
+                width: 100%;
             }
             .form-row {
                 display: flex;
@@ -120,6 +165,11 @@
             }
             
         }}
+        .color-btn {
+            width: 100%;
+            background-color: $brand-main-color;
+        }
+      
     }
     
 </style>
